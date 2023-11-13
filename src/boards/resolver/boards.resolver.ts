efa49017fa1545e10a9a0fbab33c17c553668672
boseung2 <jungbs3726@naver.com>
2023-11-14 01:09:19 +0900

    Feat: Board 페이지네이션

--- src/boards/resolver/boards.resolver.ts
+++ src/boards/resolver/boards.resolver.ts
@@ -1,8 +1,26 @@
-import { Query, Resolver, Root, ResolveField } from '@nestjs/graphql';
+import {
+  Query,
+  Resolver,
+  Root,
+  ResolveField,
+  ObjectType,
+  Field,
+  Int,
+  Args,
+} from '@nestjs/graphql';
 import { Board } from '../entity/board.entity';
 import User from 'src/users/entity/user.entity';
 import { BoardsService } from '../service/boards.service';
 import { UsersService } from '../../users/service/users.service';
+
+@ObjectType()
+class PaginatedBoards {
+  @Field(() => [Board])
+  boards: Board[];
+
+  @Field(() => Int, { nullable: true })
+  cursor?: Board['id'] | null;
+}
 
 @Resolver(Board)
 export class BoardsResolver {
@@ -11,9 +29,14 @@ export class BoardsResolver {
     private usersService: UsersService,
   ) {}
 
-  @Query(() => [Board])
-  boards(): Board[] {
-    return this.boardService.getBoards();
+  @Query(() => PaginatedBoards)
+  boards(
+    @Args('limit', { type: () => Int, nullable: true, defaultValue: 6 })
+    limit: number,
+    @Args('cursor', { type: () => Int, nullable: true, defaultValue: 1 })
+    cursor: Board['id'],
+  ): PaginatedBoards {
+    return this.boardService.getBoards(limit, cursor);
   }
 
   @ResolveField(() => User)
--- src/boards/service/boards.service.ts
+++ src/boards/service/boards.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
-import { BoardsData } from '../entity/boardsData';
+import { boardsData } from '../entity/boardsData';
 
 @Injectable()
 export class BoardsService {
-  getBoards() {
-    return BoardsData;
+  getBoards(limit: number, cursor: number) {
+    const realLimit = Math.min(6, limit);
+
+    if (!cursor) return { boards: [] };
+
+    const cursorDataIndex = boardsData.findIndex(
+      (board) => board.id === cursor,
+    );
+
+    if (cursorDataIndex === -1) return { boards: [] };
+
+    const result = boardsData.slice(
+      cursorDataIndex,
+      cursorDataIndex + realLimit,
+    );
+
+    const nextCursor = result[result.length - 1].id + 1;
+
+    const hasNext =
+      boardsData.findIndex((board) => board.id === nextCursor) > -1;
+
+    return {
+      cursor: hasNext ? nextCursor : null,
+      boards: result,
+    };
   }
 }
