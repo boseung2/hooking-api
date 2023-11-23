cf508e97b962ac2fd1e451b400a93080d8fc56b8
boseung2 <jungbs3726@naver.com>
2023-11-23 17:42:47 +0900

    Setting: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,14 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://www.hookingcommunity.com',
+      'https://www.hookingocmmunity.com',
+    ],
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(4000);
 }
